643a898b3b231ef26d30adb481c265a456d33b93
Eleazar <eleazarsaavedra@gmail.com>
2017-09-28 12:09:03 -0430

    git sencillo

--- js/alertsencillo.js
+++ js/alertsencillo.js
@@ -6,11 +6,11 @@
 var mensaje =  "Hola mundo! \n Qué facil es incluir 'comillas simples' y \"comillas dobles\"";
 
 console.log(mensaje);
-
+//arreglo
 var meses = [' Enero', ' Febrero', ' Marzo', ' Abril', ' Mayo', ' Junio', ' Julio', ' Agosto', ' Septiembre', ' Octubre', ' Noviembre', ' Diciembre'];
 
 console.log(meses);
-
+//incremento o decremento
 var numero = 5;
 ++numero;
 
@@ -19,3 +19,9 @@ console.log(numero);
 var numeroless = --numero;
 
 console.log(numeroless);
+
+//negacion
+var visible = true;
+console.log(!visible);
+
+//
