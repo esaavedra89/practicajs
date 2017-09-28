42efad5c6125c32eaef2695b999c5a46824010d4
Eleazar <eleazarsaavedra@gmail.com>
2017-09-28 15:09:30 -0430

    practica de operadores and, or y matematicos

--- js/alertsencillo.js
+++ js/alertsencillo.js
@@ -24,4 +24,27 @@ console.log(numeroless);
 var visible = true;
 console.log(!visible);
 
-//
+//and
+var valor1 = false;
+var valor2 = false;
+resultado = valor1 && valor2;
+console.log(resultado);
+
+//or
+var valor3 = false;
+var valor4 = false;
+resultado2 = valor3 || valor4;
+console.log(resultado2);
+
+//matematicos
+var numero5 = 10;
+var numero6 = 5;
+
+resultado3 = numero5 + numero6;
+resultado4 = numero5 - numero6;
+resultado5 = numero5 * numero6;
+resultado6 = numero6 / numero6;
+resultado7 = numero5 % numero6;
+
+console.log("Las operaciones son: \n" + "Suma: " + resultado3 + "\nResta: " + resultado4 + "\nMultiplicacion: " + resultado5 +
+"\nDivision: " + resultado6 + "\nModulo: " + resultado7);
