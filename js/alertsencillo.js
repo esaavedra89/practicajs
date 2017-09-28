03747f45389e44ffbba03db4a8d0ad1f30012a12
Eleazar <eleazarsaavedra@gmail.com>
2017-09-28 11:05:39 -0430

    practica de arreglo

--- js/alertsencillo.js
+++ js/alertsencillo.js
@@ -2,6 +2,11 @@
  * Created by Eleazar on 9/28/2017.
  */
 
+//primer commit
 var mensaje =  "Hola mundo! \n Qué facil es incluir 'comillas simples' y \"comillas dobles\"";
 
 alert(mensaje);
+
+var meses = [' Enero', ' Febrero', ' Marzo', ' Abril', ' Mayo', ' Junio', ' Julio', ' Agosto', ' Septiembre', ' Octubre', ' Noviembre', ' Diciembre'];
+
+alert(meses);
