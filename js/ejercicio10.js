33067bd6f4af2d9c58fc4730837d757d863d48ea
Eleazar <eleazarsaavedra@gmail.com>
2017-09-29 16:50:45 -0430

    ejercicios 10, falta resolver este ejercicio

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -1,12 +1,42 @@
 /**
  * Created by Eleazar on 9/29/2017.
  */
-//recibimos la cedena
+/*//recibimos la cedena
 var cadenaoriginal = prompt("Introduce una palabra");
 //transformamos a minisculas
 var cadenalower = cadenaoriginal.toLowerCase();
 //convertimos en un arreglo
-var cadenarreglo = cadenalower.split();
+var cadenarreglo = cadenalower.split();*/
+
+//creamos un elemento
+window.onload = function () {
+    //creamos un elemento
+    var parrafo = document.createElement("p");
+    // Crear nodo de tipo Text
+    var contenido = document.createTextNode("Hola mundo mio");
+    // Añadir el nodo Text como hijo del nodo Element
+    parrafo.appendChild(contenido);
+    // Añadir el nodo Element como hijo de la pagina
+    document.body.appendChild(parrafo);
+
+    var enlace = document.getElementById("enlace");
+    alert(enlace.href);
+
+    var imagen = document.getElementById("imagen");
+    alert(imagen.style.margin);
+
+
+};
+
+
+function borrar() {
+    //remover un nodo
+    var para = document.getElementById("provisional");
+    para.parentNode.removeChild(para);
+};
+
+
+
 
 
 
