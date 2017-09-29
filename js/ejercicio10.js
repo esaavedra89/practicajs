deef45f8240da46d5685d5a5c1ba991480fb9123
Eleazar <eleazarsaavedra@gmail.com>
2017-09-29 11:58:21 -0430

    Falta terminar

---
+++ js/ejercicio10.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Eleazar on 9/29/2017.
+ */
+//recibimos la cedena
+var cadenaoriginal = prompt("Introduce una palabra");
+//transformamos a minisculas
+var cadenalower = cadenaoriginal.toLowerCase();
+//convertimos en un arreglo
+var cadenarreglo = cadenalower.split();
+
+
+
+
+
