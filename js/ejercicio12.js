9f29a7a9504f9fd763f167dab0d3075f5f7c3606
Eleazar <eleazarsaavedra@gmail.com>
2017-10-02 11:19:15 -0430

    commit ejercicio 12

---
+++ js/ejercicio12.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Eleazar on 10/2/2017.
+ */
+function muestra() {
+    var elemento = document.getElementById("adicional");
+    elemento.className = "visible";
+
+    var enlace = document.getElementById("enlace");
+    enlace.className = "oculto";
+}
