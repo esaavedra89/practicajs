c26cf114d90382125940b72d5d58cc0b2df573a3
Eleazar <eleazarsaavedra@gmail.com>
2017-10-02 10:30:27 -0430

    commit ejercicio 11

--- js/ejercicio11.js
+++ js/ejercicio11.js
@@ -1,5 +1,9 @@
 /**
  * Created by Eleazar on 9/29/2017.
  */
+var num = 50;
+var dias = ["Lunes ","Martes ","Miercoles ","Jueves ","Viernes ","Sabado ","Domingo "]
 
-var enlaces =
+for(i=0; i<dias.length; i++){
+    document.write(dias[i] + "<br>");
+}
