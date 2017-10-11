2a8feb86d644ef82e417dbce3fb9a8c5a41894b9
Eleazar <eleazarsaavedra@gmail.com>
2017-10-11 14:02:22 -0430

    segundo push practica2 y modificacion de ejercicio 18, se agrego sweetalert

--- js/practica2.js
+++ js/practica2.js
@@ -18,7 +18,7 @@ function change() {
 }
 //cambiar imagenes dentro del html
 //es necesario dejar la variable img como variable pivada, de otra manera no funciona
-function turnOn(){
+/*function turnOn(){
     var img = document.getElementById("myImg");
     img.src='img/turn-on.jpg';
 }
@@ -27,19 +27,64 @@ function turnOff() {
     var img = document.getElementById("myImg");
     img.src ='img/turnoff.jpg';
 }
+*/
+function turnOn(){
+    var img = document.getElementById("myImg");
+    if(img.src=='img/turnoff.jpg' ||img.style.width=='100px'){
+        img.src='img/turn-on.jpg';
+    }else{
+        img.src='img/turnoff.jpg';
+    }
+
+}
+
+
 
 //change CSS styles
 function changeCSS() {
     var box_1 = document.getElementById("div-1");
     var box_2 = document.getElementById("div-2");
     var box_3 = document.getElementById("div-3");
-        box_1.className = "box1";
-        box_2.className = "box2";
-        box_3.className = "box3";
+
+
+            box_1.className = "box1";
+            box_2.className = "box2";
+            box_3.className = "box3";
+
 }
+//hide elements
 function hideElements() {
     var hide = document.getElementById("hide");
-    if(hide.style.display=="" || hide.style.display=="block"){
-        hide.style.display ="none";
-    }else {hide.style.display=="block"}
+    if(hide.style.display=='' || hide.style.display=='block'){
+        hide.style.display ='none';
+    }else {hide.style.display='block'}
 }
+//create an object
+function insert() {
+
+var demo = document.getElementById("demo");
+    var demo2 = document.getElementById("demo2");
+var person = {
+    firstName: "Eleazar",
+    lastName: "Saavedra",
+    age: 28,
+    colorSkin: "brown",
+    fullName : function() {
+        return this.firstName + " " + this.lastName;
+    }
+};
+    demo.innerHTML = person.firstName+" is a person and his lastname is "+person.lastName+
+        " and his skin color is "+person.colorSkin;
+    demo2.innerHTML = person.fullName();
+}
+
+function myFunction(name, job) {
+    var id = document.getElementById("twoArguments");
+    if(id.style.display=='' || id.style.display == 'none'){
+        id.innerHTML = "Welcome "+name+" , the "+job+".";
+    }else{
+        id.style.display= 'none';
+    }
+}
+
+    window.onload = insert;
