c1c3944f3e292479bc7d5ecb01fd7cbad2a5e55a
Eleazar <eleazarsaavedra@gmail.com>
2017-10-13 21:26:29 -0430

    Ejercicios w3school,

--- js/practica3.js
+++ js/practica3.js
@@ -1,17 +1,22 @@
 /**
  * Created by Eleazar on 10/12/2017.
  * Keep going practice with JavaScript
+ *
+ * por alguna razon, pasar las funciones dateFull() y time() al HTML como <script> no funcionaban, al regresarlas
+ * volvieron a funcionar normalmente
  */
-
+/*
 function findPosition() {
-
     var input = document.getElementById("input").value;
     var str = "With JavaScript you can find a position of one letter in an array.";
     var arr = str.split("");
     var print1 = document.getElementById("print");
 
-    print1.innerHTML = arr[input];
+    if((ci == null || ci.length == 0 || isNaN(ci)){
+        alert()
+    }else{}
 
+    print1.innerHTML = arr[input];
 }
 //convert degrees
 function convert(degree) {
@@ -24,13 +29,40 @@ function convert(degree) {
         document.getElementById("c").value = Math.round(x);
     }
 }
-
+*/
 //date
  function dateFull() {
      var date = new Date();
      var show = document.getElementById("date");
+     var day = document.getElementById("day");
+     var d;
 
      show.innerHTML = date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear();
+
+     switch (date.getDay()){
+         case 0:
+             d = "Sunday";
+             break;
+         case 1:
+             d = "Monday";
+             break;
+         case 2:
+             d = "Tuesday";
+             break;
+         case 3:
+             d = "Wednesday";
+             break;
+         case 4:
+             d = "Thursday";
+             break;
+         case 5:
+             d = "Friday";
+             break;
+         case 6:
+             d = "Satuday";
+     }
+     day.innerHTML = d;
+
  }
 
  window.onload = dateFull();
@@ -46,6 +78,7 @@ function time() {
     var timeS = document.getElementById("dtime");
     timeS.innerHTML = hour + ":" + minute + ":" + seconds;
     var t = setTimeout(time, 500);
+
 }
 function checkTime(i) {
     if (i < 10){i = "0" + i};//add zero in front of numbers <10
@@ -53,3 +86,50 @@ function checkTime(i) {
 }
 
 window.onload = time();
+
+//regard
+function regards() {
+    var sayHello = document.getElementById("sayHello");
+    var hour = new Date().getHours();
+    if(hour<12){
+        sayHello.innerHTML = "Good morming";
+    }else if(hour>12 && hour<16){
+        sayHello.innerHTML = "Good afternoon";
+    }else {
+        sayHello.innerHTML = "Good evening";
+    }
+}
+window.onload = regards();
+
+function funct() {
+    var x = "", i;
+    for (i=0; i<5; i++){
+        x = x +"The number is "+ i + "<br>";
+    }
+    document.getElementById("loop1").innerHTML = x;
+}
+
+window.onload = funct();
+
+
+function loop2() {
+    var x = "", i;
+    for (i=1; i<6; i++){
+        x = x + "<h"+i+">" + "Headings" + "</h"+i+">";
+        document.getElementById("smallerH").innerHTML = x;
+    }
+}
+
+window.onload = loop2();
+
+function whi() {
+    var text = "";
+    var i = 0;
+    while(i<10){
+        text +="<br>The number is "+i;
+        i++;
+    }
+    document.getElementById("while").innerHTML = text;
+}
+
+window.onload = whi();
